1f652ce9084b11af3c3c3e5d6dc2e7ac53bb5bec
Kegan Ronholt <kronholt273@gmail.com>
2020-02-29 19:06:55 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+//detecting button press
+var buttons = document.querySelectorAll(".drum");
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function() {
+
+    var buttonInnerHTML = this.innerHTML;
+
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+
+
+  });
+}
+
+//detecting key press
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+  buttonAnimation(event.key);
+
+});
+
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "k":
+      audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "l":
+      audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    default:
+
+  }
+
+}
+
+
+function buttonAnimation(currentKey){
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
